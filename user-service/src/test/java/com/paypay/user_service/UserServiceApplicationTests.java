245f4c4cc49f84cc6a2e26a55f36b9cd69404755
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-08-15 11:09:55 +0530

    Initial Setup

---
+++ user-service/src/test/java/com/paypay/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypay.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
